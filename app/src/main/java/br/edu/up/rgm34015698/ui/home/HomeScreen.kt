ffdf6bde4db677333540f6a92aca52f043d6552e
Jhonathan dos Santos Tomkiv <141834841+jhonazin@users.noreply.github.com>
2024-11-23 21:53:24 -0300

    Mostrando os dados

--- app/src/main/java/br/edu/up/rgm34015698/ui/home/HomeScreen.kt
+++ app/src/main/java/br/edu/up/rgm34015698/ui/home/HomeScreen.kt
@@ -39,6 +39,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
@@ -47,9 +49,11 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import br.edu.up.rgm34015698.R
 import br.edu.up.rgm34015698.InventoryTopAppBar
 import br.edu.up.rgm34015698.data.Item
+import br.edu.up.rgm34015698.ui.AppViewModelProvider
 import br.edu.up.rgm34015698.ui.navigation.NavigationDestination
 
 import br.edu.up.rgm34015698.ui.item.formatedPrice
@@ -68,10 +72,11 @@ object HomeDestination : NavigationDestination {
 fun HomeScreen(
     navigateToItemEntry: () -> Unit,
     navigateToItemUpdate: (Int) -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(factory = AppViewModelProvider.Factory)
 ) {
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
-
+    val homeUiState by viewModel.homeUiState.collectAsState()
     Scaffold(
         modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
@@ -95,7 +100,7 @@ fun HomeScreen(
         },
     ) { innerPadding ->
         HomeBody(
-            itemList = listOf(),
+            itemList = homeUiState.itemList,
             onItemClick = navigateToItemUpdate,
             modifier = modifier.fillMaxSize(),
             contentPadding = innerPadding,
